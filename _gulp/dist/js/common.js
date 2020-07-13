7c5f6cca2ce6650fa623d3c59488e911226fdaba
Dmitriy Khristoev <haossut@gmail.com>
2020-07-13 17:08:41 +0300

    new styles

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -3,24 +3,6 @@
 // Document ready
 $(document).on('ready', function(){
 
-  // E-mail Ajax Send
-  // Documentation & Example: https://github.com/agragregra/uniMail
-  $("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function() {
-      alert("Thank you!");
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-
   // Magnific popup gallery
   $('.gallery').each(function() {
     $(this).magnificPopup({
@@ -28,21 +10,6 @@ $(document).on('ready', function(){
       type: 'image',
       gallery:{
         enabled:true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
       }
     });
   });
@@ -81,78 +48,9 @@ $(document).on('ready', function(){
   } catch(err) {
 
   };
-
-  // simpleForm version 2015-09-23 14:30 GMT +2
-  simpleForm('form.form-callback');
 });
 
-$(window).on('load', function() {
-  $(".loader").delay(400).fadeOut("slow");
-});
+$(window).on('load', function() { });
 
 $(window).on('scroll', function() { });
 $(window).on('resize', function() { });
-
-/*
-version 2015-09-23 14:30 GMT +2
-*/
-function simpleForm(form, callback) {
-  $(document).on('submit', form, function(e){
-    e.preventDefault();
-    var formData = {};
-    var hasFile = false;
-    if ($(this).find('[type=file]').length < 1) {
-      formData = $(this).serialize();
-    }
-    else {
-      formData = new FormData();
-      $(this).find('[name]').each(function(){
-
-        switch($(this).prop('type')) {
-
-          case 'file':
-            if ($(this)[0]['files'].length > 0) {
-              formData.append($(this).prop('name'), $(this)[0]['files'][0]);
-              hasFile = true;
-            }
-            break;
-
-          case 'radio':
-          case 'checkbox':
-            if (!$(this).prop('checked')) {
-              break;
-            }
-            formData.append($(this).prop('name'), $(this).val().toString());
-            break;
-
-          default:
-            formData.append($(this).prop('name'), $(this).val().toString());
-            break;
-        }
-      });
-    }
-
-    $.ajax({
-      url: $(this).prop('action'),
-      data: formData,
-      type: 'POST',
-      contentType : hasFile ? 'multipart/form-data' : 'application/x-www-form-urlencoded',
-      cache       : false,
-      processData : false,
-      success: function(response) {
-        $(form).removeClass('ajax-waiting');
-        $(form).find("[type=submit]").prop("disabled", false);
-        $(form).html($(response).find(form).html());
-
-        if (typeof callback === 'function') {
-          callback(response);
-        }
-      }
-    });
-
-    $(form).addClass('ajax-waiting');
-    $(form).find("[type=submit]").prop("disabled", true);
-
-    return false;
-  });
-}

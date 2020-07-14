bf5fb34b332713499566bf182e4fad091cafcba3
Dmitriy Khristoev <haossut@gmail.com>
2020-07-14 14:00:58 +0300

    new styles

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -39,6 +39,9 @@ $(document).on('ready', function(){
     midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
   });
 
+  // Footer menu mobile animation
+  footerNav();
+
   // Chrome Smooth Scroll
   try {
     $.browserSelector();
@@ -53,4 +56,32 @@ $(document).on('ready', function(){
 $(window).on('load', function() { });
 
 $(window).on('scroll', function() { });
-$(window).on('resize', function() { });
+$(window).on('resize', function() {
+  var width = $(window).width();
+  
+  if (width >= 992) {
+    $('.j-footer-nav').removeClass('is-active');
+    $('.footer__nav').attr('style','');
+  }
+});
+
+function footerNav() {
+  var link = $('.j-footer-nav');
+  
+  link.each(function(){
+    var _this = $(this);
+    
+    _this.on('click', function(){
+      var width = $(window).width();
+      if (_this.hasClass('is-active') && width <= 991) {
+        _this.removeClass('is-active');
+        _this.next('.footer__nav').slideUp();
+      } else if (!_this.hasClass('is-active') && width <= 991) {
+        _this.addClass('is-active');
+        _this.next('.footer__nav').slideDown();
+      } else if (width >= 992) {
+        return false
+      }
+    });
+  })
+}
